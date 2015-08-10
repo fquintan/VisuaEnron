be471c43c68a23a609e2f1f982469752c7baaa5e
Sebastian Ferrada <scferradaa@gmail.com>
2015-08-10 13:57:58 -0300

    date added to json

--- cadenas/detect_threads.py
+++ cadenas/detect_threads.py
@@ -8,6 +8,16 @@ STOPWORDS = set(['thanks', 'forwarded', 'would', 'please', 'subject', 'want', 'i
 def print_to_err(*objs):
     print(*objs, file=sys.stderr)
 	
+def jsonify(emails):
+	json = '['
+	punctuation_map = string.maketrans(string.punctuation, ' '*len(string.punctuation))
+	for email in emails:
+		json += '{'
+		json += '\"text\":\"' + email['text'].translate(punctuation_map).strip() + '\",'
+		json += '\"date\":\"' + str(email['date']) + '\"'
+		json += '},\n'
+	return json + ']'
+	
 def load_enron_emails(folder=''):
 	# clear_subspace(db, ER_space)
 	# reader = EmailWalker(localdir+folder)
@@ -15,7 +25,6 @@ def load_enron_emails(folder=''):
 	count_mails = 0	
 	punctuation_map = string.maketrans(string.punctuation, ' '*len(string.punctuation))
 	res_strings = ['re:', 'fw:', 'fwd:']
-	#max = 100
 	threads = {}
 	for email in reader:
 		subject = email['subject'].lower()
@@ -25,23 +34,22 @@ def load_enron_emails(folder=''):
 		if subject == '':
 			continue
 		if subject in threads:
-			threads[subject].append(email['text'].translate(punctuation_map).strip())
+			threads[subject].append(email)
 		else:
-			threads[subject] = [email['text'].translate(punctuation_map).strip()]
+			threads[subject] = [email]
 		if (count_mails % 1000) == 0:
 			print ("[%s] %s" % (datetime.datetime.utcnow(),count_mails))
-		#if count_mails > max:
-		#	break
+
 		count_mails +=1
 	print ("[%s] Finished processing mails, started writing" % datetime.datetime.utcnow())
 	
 	#import operator
 	#sorted_dict = sorted(hist.items(), key=operator.itemgetter(1), reverse=True)
 	
-	out = open("threads.json", "w")
+	out = open("threads_dated.json", "w")
 	out.write("data=[")
 	for (k, v) in threads.iteritems():
-		out.write("{\"subject\":\"%s\", \"mails\":\"%s\", \"size\":%s},\n" % (k, '\n'.join(v), len(v)))
+		out.write("{\"subject\":\"%s\", \"mails\":\"%s\", \"size\":%s},\n" % (k, jsonify(v), len(v)))
 	out.write("];")
 	out.close()
 
